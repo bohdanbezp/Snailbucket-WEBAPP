1ccefcd003ff1c985b1418d1054e7beb8f699ecb
Bodia <bvk256@gmail.com>
2009-12-15 15:53:11 +0200

    build updated

--- build.sh
+++ build.sh
@@ -6,13 +6,14 @@ cd cache
 
 # download dependencies
 wget http://gwtwiki.googlecode.com/files/bliki.3.0.13.zip
-wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.2.6.zip
+wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.3.0.zip
 wget http://mirrors.ibiblio.org/pub/mirrors/maven2/commons-fileupload/commons-fileupload/1.2.1/commons-fileupload-1.2.1.jar
 wget http://mirrors.ibiblio.org/pub/mirrors/maven2/org/apache/commons/commons-io/1.3.2/commons-io-1.3.2.jar
+wget http://prdownloads.sourceforge.net/nanoxml/nanoxml-lite-2.2.1.jar
 
 # unzipping the GAE SDK and bliki
 echo "unpacking AppEngine SDK..."
-unzip -d ../ appengine-java-sdk-1.2.6.zip > /dev/null
+unzip -d ../ appengine-java-sdk-1.3.0.zip > /dev/null
 echo "unpacking bliki..."
 unzip -j -d ../war/WEB-INF/lib bliki.3.0.13.zip info.bliki.wiki/bliki-core/target/bliki-core-3.0.13.jar > /dev/null
 
