31edb157a37bca6458ee88cd9f9684c5a0894483
Bogdan Bezpartochnyi <bvk256@gmail.com>
2014-11-20 01:00:55 +0200

    Sendgrid executor improved

--- src/main/java/net/rwchess/utils/SendgridMailer.java
+++ src/main/java/net/rwchess/utils/SendgridMailer.java
@@ -1,7 +1,16 @@
 package net.rwchess.utils;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -15,28 +24,28 @@ public class SendgridMailer implements Mailer {
     private ExecutorService taskExecutor = Executors.newFixedThreadPool(4);
 
     @Override
-    public void sendEmail(String from, String subject, String contents, String to) {
+    public void sendEmail(final String from, final String subject, final String contents, final String to) {
         taskExecutor.execute(new Runnable() {
             @Override
             public void run() {
-                //        HttpClient httpclient = new DefaultHttpClient();
-//        HttpPost get = new HttpPost("https://sendgrid.com/api/mail.send.json");
-//        log.info("Preparing email to " + to);
-//        try {
-//            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-//            nameValuePairs.add(new BasicNameValuePair("to", to));
-//            nameValuePairs.add(new BasicNameValuePair("toname", "Member"));
-//            nameValuePairs.add(new BasicNameValuePair("subject", subject));
-//            nameValuePairs.add(new BasicNameValuePair("text", contents));
-//            nameValuePairs.add(new BasicNameValuePair("from", from));
-//            nameValuePairs.add(new BasicNameValuePair("api_user", "bvk256"));
-//            nameValuePairs.add(new BasicNameValuePair("api_key", "bodiaissendgrid"));
-//            get.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-//            HttpResponse r = httpclient.execute(get);
-//            log.info("Response " + r.toString());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost get = new HttpPost("https://sendgrid.com/api/mail.send.json");
+                log.info("Preparing email to " + to);
+                try {
+                    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                    nameValuePairs.add(new BasicNameValuePair("to", to));
+                    nameValuePairs.add(new BasicNameValuePair("toname", "Member"));
+                    nameValuePairs.add(new BasicNameValuePair("subject", subject));
+                    nameValuePairs.add(new BasicNameValuePair("text", contents));
+                    nameValuePairs.add(new BasicNameValuePair("from", from));
+                    nameValuePairs.add(new BasicNameValuePair("api_user", "bvk256"));
+                    nameValuePairs.add(new BasicNameValuePair("api_key", "bodiaissendgrid"));
+                    get.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse r = httpclient.execute(get);
+                    log.info("Response " + r.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
